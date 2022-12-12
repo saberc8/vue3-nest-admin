b5c63916945acfb14e4108023c8e845416de6000
saber <1558365539@qq.com>
2022-12-12 18:48:54 +0800

    feat: server 环境配置

--- server/README.md
+++ server/README.md
@@ -1,31 +1,3 @@
-## Installation
+rbac服务端
 
-```bash
-$ pnpm install
-```
-
-## Running the app
-
-```bash
-# development
-$ pnpm run start
-
-# watch mode
-$ pnpm run start:dev
-
-# production mode
-$ pnpm run start:prod
-```
-
-## Test
-
-```bash
-# unit tests
-$ pnpm run test
-
-# e2e tests
-$ pnpm run test:e2e
-
-# test coverage
-$ pnpm run test:cov
-```
+项目结构目录
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { getConfig } from './utils'
+
+export const config = getConfig()
+const PORT = config.PORT || 4000
+const PREFIX = config.PREFIX || '/'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // 打印4000端口访问地址
-  console.log(`http://localhost:4000`);
-  await app.listen(4000);
+  console.log(getConfig, '--')
+  const app = await NestFactory.create(AppModule)
+
+  //允许跨域请求
+  app.enableCors()
+
+  // 设置请求前缀
+  app.setGlobalPrefix(PREFIX)
+  console.log(`http://localhost:${PORT}/${PREFIX}`)
+  await app.listen(PORT)
 }
-bootstrap();
+
+bootstrap()
