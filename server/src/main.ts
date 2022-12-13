2adfc687665d384944cc00ae8a8574c5eb59f7b6
saber <1558365539@qq.com>
2022-12-13 13:47:01 +0800

    feat: 默认页

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,33 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger, VersioningType } from '@nestjs/common'
-import { ApiModule } from './api/api.module'
-import { getConfig, IS_DEV } from './utils'
 
+import { AppModule } from './app.module'
+import { getConfig, IS_DEV } from './utils'
 export const config = getConfig()
 const PORT = config.PORT || 4000
 const PREFIX = config.PREFIX || '/'
-
 async function bootstrap() {
   const logger: Logger = new Logger('main.ts')
-  const app = await NestFactory.create(ApiModule, {
+  const app = await NestFactory.create(AppModule, {
     // 开启日志级别打印
     logger: IS_DEV ? ['log', 'debug', 'error', 'warn'] : ['error', 'warn'],
   })
-
   //允许跨域请求
   app.enableCors()
-
-  // 启动版本管理
-  app.enableVersioning({
-    defaultVersion: 'v1.0', // 不指定默认版本为v1
-    type: VersioningType.URI,
-  })
-
   // 设置请求前缀
   app.setGlobalPrefix(PREFIX)
   await app.listen(PORT, () => {
     logger.log(`Server running on http://localhost:${PORT}/${PREFIX}`)
   })
 }
-
 bootstrap()
