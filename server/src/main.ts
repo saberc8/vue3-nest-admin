a65191da311774b7326fbf7814479ed70ee0535b
saber <1558365539@qq.com>
2022-12-13 11:01:36 +0800

    feat: update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger, VersioningType } from '@nestjs/common'
 import { ApiModule } from './api/api.module'
-import { getConfig } from './utils'
+import { getConfig, IS_DEV } from './utils'
 
 export const config = getConfig()
 const PORT = config.PORT || 4000
 const PREFIX = config.PREFIX || '/'
 
 async function bootstrap() {
-  console.log(getConfig, '--')
-  const app = await NestFactory.create(ApiModule)
+  const logger: Logger = new Logger('main.ts')
+  const app = await NestFactory.create(ApiModule, {
+    // 开启日志级别打印
+    logger: IS_DEV ? ['log', 'debug', 'error', 'warn'] : ['error', 'warn'],
+  })
 
   //允许跨域请求
   app.enableCors()
@@ -23,7 +26,9 @@ async function bootstrap() {
   // 设置请求前缀
   app.setGlobalPrefix(PREFIX)
   console.log(`http://localhost:${PORT}/${PREFIX}`)
-  await app.listen(PORT)
+  await app.listen(PORT, () => {
+    logger.log(`Server running on http://localhost:${PORT}/${PREFIX}`)
+  })
 }
 
 bootstrap()
