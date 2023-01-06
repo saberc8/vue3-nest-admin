400c1217e26f78a4af5c8fdddefb791bfc5beeff
saber <1558365539@qq.com>
2023-01-06 17:28:07 +0800

    feat: add role&menu

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { LoginModule } from './login/login.module'
 import { UserModule } from './user/user.module'
+import { MenuModule } from './menu/menu.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [LoginModule, UserModule],
+  imports: [LoginModule, UserModule, MenuModule, RoleModule],
 })
 export class ApiModule {}
