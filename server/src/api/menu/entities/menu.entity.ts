0f6fae4a2f027e7d5c5d343de3ce07d6196d3aac
saber <1558365539@qq.com>
2023-01-06 18:02:04 +0800

    feat: 菜单表

--- server/src/api/menu/entities/menu.entity.ts
+++ server/src/api/menu/entities/menu.entity.ts
@@ -1,1 +1,50 @@
-export class Menu {}
+import { SharedEntity } from '@src/shared/entities/base.entity'
+import { Entity, Column, Index } from 'typeorm'
+
+@Entity('menu')
+export class MenuEntity extends SharedEntity {
+  constructor() {
+    super()
+  }
+
+  @Index()
+  @Column({
+    type: 'varchar',
+    length: 50,
+    name: 'name',
+    comment: '菜单名称',
+  })
+  name!: string
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    name: 'path',
+    comment: '菜单路径',
+  })
+  path!: string
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    name: 'icon',
+    comment: '菜单图标',
+  })
+  icon!: string
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    name: 'component',
+    comment: '菜单组件',
+  })
+  component!: string
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    name: 'redirect',
+    comment: '菜单重定向',
+  })
+  redirect!: string
+}
